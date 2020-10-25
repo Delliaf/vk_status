2e927f79ec6ecba113ab3243a51d5b6745c7a4f1
Dennis <blinhic850@mail.ru>
2020-10-25 12:27:58 +0300

    Привёл в порядок код

--- vkstatusupd.py
+++ vkstatusupd.py
@@ -3,40 +3,32 @@ import datetime
 import vk_api
 
 
-def auth_handler():
-    """ При двухфакторной аутентификации вызывается эта функция.
-    """
-
-    # Код двухфакторной аутентификации
-    key = input("Enter authentication code: ")
-    # Если: True - сохранить, False - не сохранять.
-    remember_device = True
-
+def auth_handler(): #если есть двухфакторная аутентификция, то вызывается эта функция
+    key = input("Enter authentication code: ") #код аутентификации
+    remember_device = True # если: True - сохранить, False - не сохранять
     return key, remember_device
 
 
 def main():
-    """ Пример обработки двухфакторной аутентификации """
-
     login, password = 'логин', 'пароль'
     vk_session = vk_api.VkApi(
         login, password,
         # функция для обработки двухфакторной аутентификации
-        auth_handler=auth_handler
+        auth_handler = auth_handler
     )
 
     try:
         def countdown(stop):
             while True:
                 vk_session.auth()
-                delta = datetime.timedelta(hours=3, minutes=0)
+                delta = datetime.timedelta(hours = 3)
                 t = (datetime.datetime.now(datetime.timezone.utc) + delta)
-                nowtime = t.strftime("%H:%M")
-                nowdate = t.strftime("%d.%m.%Y")
-                on = vk_session.method("friends.getOnline")
-                counted = len(on)
+                nowtime = t.strftime("%H:%M") #текущее время
+                nowdate = t.strftime("%d.%m.%Y") #текущая дата
+                on = vk_session.method("friends.getOnline") 
+                counted = len(on) #количество друзей в онлайне
                 vk_session.method("status.set", {"text": "Сейчас в Москве: " + nowtime})
-                time.sleep(2*60)
+                time.sleep(2 * 60) #время в минутах для автообновления статуса (без капчи - от двух-трёх минут и выше
 
         end_time = datetime.datetime(9999, 9, 1, 0, 0, 0)
         countdown(end_time)
